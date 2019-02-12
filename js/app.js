4d0a250fc944b3694398dc05c48111ff1e91ddce
Shawn Gilroy <shawn.gilroy@temple.edu>
2019-02-11 20:42:56 -0600

    update

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//(function() {
+    // init db
+    var config = {
+        apiKey: "AIzaSyAe1F5zmD2UEopduuroDDQ6opPYWyquJvQ",
+        authDomain: "visual-discrimination-app.firebaseapp.com",
+        databaseURL: "https://visual-discrimination-app.firebaseio.com",
+        projectId: "visual-discrimination-app",
+        storageBucket: "visual-discrimination-app.appspot.com",
+        messagingSenderId: "313567895439"
+    };
+
+    firebase.initializeApp(config);
+
+//})
