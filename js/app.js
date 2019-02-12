6fd9e45d27ccf8d0fcaac77a06bd1bb740957b32
Shawn Gilroy <shawn.gilroy@temple.edu>
2019-02-11 20:46:10 -0600

    update

--- js/app.js
+++ js/app.js
@@ -9,6 +9,21 @@
         messagingSenderId: "313567895439"
     };
 
+    var firestore = firebase.firestore();
+
+    const docRef = firestore.doc("storage");
+
     firebase.initializeApp(config);
 
+    getRealtimeUpdates = function() {
+        docRef.onSnapshot(function (doc) {
+            if (doc && doc.exists) {
+                const myData = doc.data();
+
+                console.log(myData);                
+            }
+        });
+    }
+
+    getRealtimeUpdates();
 //})
